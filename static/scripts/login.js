9ded27db07125580bbfc63f0fe26fe2577194bf0
Jee Young Rebecca Park <jyrebecca@Jees-MacBook-Pro.local>
2016-08-21 10:57:56 -0400

    Enabled hiding on login.html

--- static/scripts/login.js
+++ static/scripts/login.js
@@ -1,5 +1,6 @@
 var num_star=0;
 var c_num_star = document.getElementById("num-star");
+var starredPostClassName = document.querySelector(".starred-post")
 
 function setOnclick(stars, i) {
 	var star = stars[i];
@@ -19,21 +20,39 @@ function setOnclick(stars, i) {
 
 function display_num_star(num_star) {
 	if(num_star == 0) {
-		c_num_star.innerText = "";
+		starredPostClassName.className = "btn btn-white starred-post hidden";
 	} else {
-	c_num_star.innerText = num_star;
+		starredPostClassName.className = "btn btn-white starred-post";
+		c_num_star.innerText = num_star;
 	}
 }
 
 var stars = document.querySelectorAll(".keyword-light-purple");
-
 for (var i = 0; i < stars.length; i++) {
 	setOnclick(stars, i);
 }
 
+var buttons = document.querySelectorAll(".fa-close");
+var rows = document.querySelectorAll(".posting-row");
+for (var i = 0; i < buttons.length; i++) {
+	setOnClickHideRow (buttons, rows, i);
+}
+
+function setOnClickHideRow (buttons, rows, i) {
+	var button = buttons[i];
+	var row = rows[i];
+	button.onclick = function() {
+		row.className = "posting-row row hidden";
+		console.log("here");
+	}
+}
+
 var user = document.querySelector(".user");
-user.onclick = function () {
-	document.querySelector(".user-drop-down").className = "user-drop-down";
+if (user == null) {
+} else {
+	user.onclick = function () {
+		document.querySelector(".user-drop-down").className = "user-drop-down";
+	}
 }
 
 var user_drop = document.querySelector(".user-drop-down");
